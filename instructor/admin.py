d83730d1f321c068ec978b181a6c00ec8a86e3d7
mumiaktech <akoth0mitch@gmail.com>
2025-01-28 15:26:15 +0300

    Getting ready to create a learning management system

--- instructor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Resource, Report, UserProfile, Activity, Notification
-
-admin.site.register(Report)
-admin.site.register(Resource)
-admin.site.register(UserProfile)
-admin.site.register(Activity)
-admin.site.register(Notification)
--- oqms/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Question, Quiz, QuizAttempt, Answer, StudentAnswer
-
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Quiz)
-admin.site.register(QuizAttempt)
-admin.site.register(Answer)
-admin.site.register(StudentAnswer)
